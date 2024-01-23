12862bb831e9738b6c2fb4aff1564aeae9c20713
abayojosephdesire <67261480+abayojosephdesire@users.noreply.github.com>
2024-01-23 18:10:41 -0500

    Update order.js

--- server/models/order.js
+++ server/models/order.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 //Order schema
 const OrderSchema = new mongoose.Schema({
+  creator_id: String,
+  creator_name: String,
   content: String,
 });
 module.exports = mongoose.model("order", OrderSchema);
