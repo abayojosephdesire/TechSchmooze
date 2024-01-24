b535704a7725acad73b3f5834ccb4506e35c4bd5
abayojosephdesire <67261480+abayojosephdesire@users.noreply.github.com>
2024-01-24 14:11:55 -0500

    Create CardOrder.js

---
+++ client/src/components/modules/CardOrder.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react";
+import SingleOrder from "./SingleOrder.js";
+import { get } from "../../utilities";
+import "./CardOrder.css";
+
+const CardOrder = (props) => {
+  return (
+    <div>
+      <SingleOrder
+        _id={props._id}
+        creator_name={props.creator_name}
+        creator_id={props.creator_id}
+        content={props.content}
+      />
+    </div>
+  );
+};
+export default CardOrder;
